9ea5777d371a025b650e981473c1772f84af24a1
charlescai <charlescai.hku@gmail.com>
2019-01-18 14:14:26 -0500

    Structure of the code

---
+++ sample_code_titanic.R
@@ -0,0 +1,15 @@
+#1. import data
+train <- read.csv("/Users/charlescai/Documents/titanic/all/train.csv")
+submission <- read.csv("/Users/charlescai/Documents/titanic/all/test.csv")
+
+#2. visualize data
+
+#3. clean data
+
+#4. split train and test set
+
+#5. different models and evaluation of models
+
+#6. repeat 4 and 5, and choose the model
+
+#7. produce the submission file
