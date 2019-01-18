262f7cbded85bf03ab012399805d2222f4f8a06c
charlescai <charlescai.hku@gmail.com>
2019-01-18 15:10:22 -0500

    added sample_code

--- sample_code_titanic.R
+++ sample_code_titanic.R
@@ -4,6 +4,15 @@ submission <- read.csv("/Users/charlescai/Documents/titanic/all/test.csv")
 
 #2. visualize data
 
+#2.1 know the variables
+str(train)
+str(submission)
+
+#2.2 find out if there're missing values
+pacman::p_load(ggplot2)
+is.na(train)
+
+
 #3. clean data
 
 #4. split train and test set
